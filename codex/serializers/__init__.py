e1cff297d68c77d2c93c45da2d2154b0ee0f6323
Juliovaz <julio.oliveiravaz0@gmail.com>
2021-10-06 23:05:27 -0300

    feat: initial implementation of authentication

--- codex/serializers/__init__.py
+++ codex/serializers/__init__.py
@@ -1,5 +1,7 @@
 from codex.serializers.perfil_serializer import PerfilSerializer
+from codex.serializers.auth_serializer import CustomTokenObtainPairSerializer
 
 __all__ = [
-    'PerfilSerializer'
+    'PerfilSerializer',
+    'CustomTokenObtainPairSerializer'
 ]
