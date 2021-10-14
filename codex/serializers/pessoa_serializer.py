4e869d20cbc3cda26fec52594a9ea56f8de3a71e
Juliovaz <julio.oliveiravaz0@gmail.com>
2021-10-13 21:42:57 -0300

    refactor: try to adjust fields into serializer

--- codex/serializers/pessoa_serializer.py
+++ codex/serializers/pessoa_serializer.py
@@ -1,16 +1,22 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
-from codex.models import perfil
 from codex.models.pessoa import Pessoa
 from codex.models.perfil import Perfil
 from django.contrib.auth.hashers import make_password
 
-#Serializer
 class PessoaSerializer(serializers.ModelSerializer):
+    # id= serializers.IntegerField(read_only=True)
+    # nome= serializers.CharField(required=True)
+    # email= serializers.CharField(required=True)
+    # senha= serializers.CharField(required=True)
+    # cpf= serializers.CharField(required=True)
+    # data_nascimento= serializers.DateTimeField(required=True)
+    # telefone= serializers.CharField(required=False, allow_blank=True)
+    # celular= serializers.CharField(required=True)
+    # ativo= serializers.BooleanField(default='1')
+    # perfil= serializers.IntegerField(required=True)
     class Meta:
         model = Pessoa
-        fields = "__all__"
-        # fields = []
+        fields = '__all__'
     
     def create(self, validated_data):
         perfil_instance = Perfil.objects.get(id=validated_data["perfil"])
