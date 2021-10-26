394d8beae514d98f7126645b375c6e6ccb1d5116
Alexandre Carvalho <alexandre.carvalho@live.com>
2021-10-26 15:20:50 -0300

    Added CRUD Armario

--- codex/urls.py
+++ codex/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from codex.views.armario_view import ArmarioView
 from codex.views.pessoa_view import PessoaView
 from codex.views.perfil_view import PerfilView
 
@@ -18,5 +18,12 @@ urlpatterns = [
     path('pessoa/detalhe/<str:pk>', PessoaView.detalhe_pessoa, name="detalhe-perfil"),
     path('pessoa/deletar/<str:pk>', PessoaView.deletar_pessoa, name="deletar-pessoa"),
     path('pessoa/atualizar', PessoaView.atualizar_pessoa, name="atualizar-pessoa"),
-    path('pessoa/lista', PessoaView.retorna_pessoas, name="retorna-pessoas")
+    path('pessoa/lista', PessoaView.retorna_pessoas, name="retorna-pessoas"),
+
+    # armario
+    path('armario/lista', ArmarioView.retorna_armarios, name="retorna-armarios"),
+    path('armario/detalhe/<str:pk>', ArmarioView.detalhe_armario, name='detalhe-armario'),
+    path('armario/salvar', ArmarioView.salvar_armario, name='salvar-armario'),
+    path('armario/atualizar/<str:pk>', ArmarioView.atualizar_armario, name='atualizar-armario'),
+    path('armario/deletar/<str:pk>', ArmarioView.deletar_armario, name='deletar-armario'),    
 ]
