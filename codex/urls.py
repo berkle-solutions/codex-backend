3f0667390e720e4ddc9d1dac3ad5950483fdd654
Juliovaz <julio.oliveiravaz0@gmail.com>
2021-10-26 19:22:53 -0300

    feat: ensure authentication of users

--- codex/urls.py
+++ codex/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from codex.views.autenticacao_view import AutenticacaoView
 
 from codex.views.pessoa_view import PessoaView
 from codex.views.perfil_view import PerfilView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
@@ -18,5 +24,9 @@ urlpatterns = [
     path('pessoa/detalhe/<str:pk>', PessoaView.detalhe_pessoa, name="detalhe-perfil"),
     path('pessoa/deletar/<str:pk>', PessoaView.deletar_pessoa, name="deletar-pessoa"),
     path('pessoa/atualizar', PessoaView.atualizar_pessoa, name="atualizar-pessoa"),
-    path('pessoa/lista', PessoaView.retorna_pessoas, name="retorna-pessoas")
+    path('pessoa/lista', PessoaView.retorna_pessoas, name="retorna-pessoas"),
+    #autenticacao
+    path('autenticacao/token', AutenticacaoView.autenticar_usuario, name="autenticacao")
+    # path('autenticacao/token', TokenObtainPairView.as_view(), name="autenticacao-token"),
+    # path('autenticacao/token/refresh/', TokenRefreshView.as_view(), name='autenticacao-token-refresh'),
 ]
