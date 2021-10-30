a5d4f9ed86b6cc40534bee72b50f9b1510b6469d
Alexandre Carvalho <alexandre.carvalho@live.com>
2021-10-30 07:51:55 -0300

    Nested relationships between Armario and compartimento

--- codex/models/compartilhamento.py
+++ codex/models/compartilhamento.py
@@ -5,7 +5,11 @@ from codex.models.armario import Armario
 class Compartilhamento(models.Model):
     descricao = models.CharField(max_length=45)
     ocupado = models.BooleanField(default=False)
-    armario = models.ForeignKey(Armario, on_delete=models.CASCADE)
+    armario = models.ForeignKey(Armario, related_name='compartimentos', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['armario', 'descricao']
+        ordering = ['descricao']
 
     def __str__(self) -> str:
-        return self.name
+        return self.name        
