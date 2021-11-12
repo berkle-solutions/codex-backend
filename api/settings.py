519c84f938935c6b5cefcd8e3089cf4ca8400ae9
Juliovaz <julio.oliveiravaz0@gmail.com>
2021-11-11 23:14:57 -0300

    disabled allow hosts

--- api/settings.py
+++ api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-vf(4-hp$s0q*=5_@h-o12*sct=8i38*1eljs6b$%%a)0h)ale7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://codex-backend-tcc.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
